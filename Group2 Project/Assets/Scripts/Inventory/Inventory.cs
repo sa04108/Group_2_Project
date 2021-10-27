f4dc7af3e1da0f039f36dc726238cbd3720db571
woohyun95 <diddngus77@naver.com>
2021-10-27 17:49:12 +0900

    [Inventory] 인벤토리 기본적인 시스템 세팅
    아이템 클래스 구체화 및 인벤토리 기능 정리 필요

--- Group2 Project/Assets/Scripts/Inventory/Inventory.cs
+++ Group2 Project/Assets/Scripts/Inventory/Inventory.cs
@@ -4,13 +4,49 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
+    #region Singleton
+    public static Inventory instance;
+    private void Awake() {
+        if(instance != null) {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+    }
+    #endregion
+
+    public delegate void OnSlotCountChange(int val);
+    public OnSlotCountChange onSlotCountChange;
+
+    public delegate void OnChangeItem();
+    public OnChangeItem onChangeItem;
+
+    private int slotCnt;
+
+    public int SlotCnt {
+        get => slotCnt;
+        set {
+            slotCnt = value;
+        }
+    }
+
+    void Start() {
+        SlotCnt = 4;
+    }
+
     public List<Item> items = new List<Item>();
     [SerializeField] public int slots;
 
-    public void GetItem(Item item) {
-        if(items.Count < slots) {
-            items.Add(item);
+
+    public bool AddItem(Item _item) {
+        if(items.Count < SlotCnt) {
+            items.Add(_item);
+            if(onChangeItem != null) {
+                onChangeItem.Invoke();
+            }
+            return true;
         }
+        return false;
     }
 
     public bool IsFull() {
@@ -18,5 +54,14 @@ public class Inventory : MonoBehaviour
             return true;
         }
         return false;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.tag == "dropItem") {
+            DropItem dropItems = other.GetComponent<DropItem>();
+            if (AddItem(dropItems.GetItem())) {
+                dropItems.DestroyItem();
+            }
+        }
     }
 }
--- Group2 Project/Assets/Scripts/Item/Item.cs
+++ Group2 Project/Assets/Scripts/Item/Item.cs
@@ -1,31 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public enum ITEM_TYPE {
     EQUIPMENT,
     RESOURCES,
     CONSUMABLES,
 }
-
-public class Item : MonoBehaviour
+[System.Serializable]
+public class Item 
 {
-    public ITEM_TYPE itemType;
-    public string itemName;
-    public Sprite itemImage;
+  public ITEM_TYPE itemType;
+  public string itemName;
+  public Sprite itemImage;
 
-    private void OnTriggerEnter(Collider other) {
-        if(other.tag == "Player") {
-            Inventory inven = GameObject.FindObjectOfType<Inventory>();
-
-            if (!inven.IsFull()) {
-                inven.GetItem(this);
-            }
-            
-        }
-    }
-
-    public void DestroyItem() {
-        Destroy(gameObject);
-    }
 }
