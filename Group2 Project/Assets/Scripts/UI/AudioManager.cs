0e2a81c6a9bb93282959b6167095634e5ac1a3ec
sa04108 <sa04108@naver.com>
2021-11-11 01:46:22 +0900

    Audio Manager

--- Group2 Project/Assets/Scripts/UI/AudioManager.cs
+++ Group2 Project/Assets/Scripts/UI/AudioManager.cs
@@ -5,22 +5,53 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager instance;
+    public bool isAlwaysShowCursor;
+
+    GameObject optionPanel;
+    bool isOptionOn;
 
     // Start is called before the first frame update
     void Start()
     {
+        isOptionOn = false;
+
         if (instance)
             Destroy(this);
         else
             instance = this;
 
         DontDestroyOnLoad(gameObject);
+
+        optionPanel = transform.Find("Option Panel").gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+            if (isOptionOn)
+                HideOptionPanel();
+            else
+                ShowOptionPanel();
+    }
+
+    public void ShowOptionPanel()
+    {
+        isOptionOn = true;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        optionPanel.SetActive(true);
+    }
+
+    public void HideOptionPanel()
+    {
+        isOptionOn = false;
+        if (!isAlwaysShowCursor)
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        optionPanel.SetActive(false);
     }
 
     public void SetVolume(float vol)
