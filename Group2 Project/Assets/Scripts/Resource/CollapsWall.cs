aabb9ff67242098fc0a4e641b71591fb6ef5cc62
kimyeonwan <dusdhks7@gmail.com>
2021-11-23 13:56:14 +0900

    벽 생성 및 돌,나무 자원화

---
+++ Group2 Project/Assets/Scripts/Resource/CollapsWall.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollapsWall : MonoBehaviour
+{
+    [SerializeField]
+    private float DestroyTime;
+
+    [SerializeField]
+    private Collider col;
+    [SerializeField]
+    private GameObject Wall;
+    [SerializeField]
+    private GameObject Wall_debris;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.tag == "Explosion")
+        {
+            Destruction();
+        }
+    }
+    private void Destruction()
+    {
+        col.enabled = false;
+
+        Destroy(Wall);
+
+        Wall_debris.SetActive(true);
+        Destroy(Wall_debris, DestroyTime);
+    }
+}
